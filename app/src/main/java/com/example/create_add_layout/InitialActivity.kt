d1fdfe61f592b15ba2aacc124e5c3db5e2cc28fe
daividvleal <daivid.v.leal@gmail.com>
2024-04-08 21:17:46 -0300

    change initial screen

---
+++ app/src/main/java/com/example/create_add_layout/InitialActivity.kt
@@ -0,0 +1,20 @@
+package com.example.create_add_layout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InitialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_initial)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
