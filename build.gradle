110e3ccfe2d511cdd45e997c9bb4d71091aad271
daividvleal <7713755+daividvleal@users.noreply.github.com>
2024-05-11 11:14:03 -0300

    initial-configuration-to-firebase

--- build.gradle
+++ build.gradle
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.parcelizeAndroid) apply false
+    alias(libs.plugins.firebaseAndroid) apply false
 }
